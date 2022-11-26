e786787a9b9e69359847a020ad697ef03a05f263
Dagim <108428899+Dagim-Zeynu@users.noreply.github.com>
2022-11-26 11:31:46 +0300

    some editing
    some editing.
    
    Co-Authored-By: Dagim <108428899+dagim-zeynu@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/test', function(){
+    return App\User::find(1)->profile;
+});
 Route::get('/', function () {
     return view('welcome');
 });
@@ -28,12 +21,43 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
         'uses'=>'PostsController@create',
         'as'=> 'post.create'
     ]);
+
     
     Route::post('/post/store', [
         'uses'=>'PostsController@store',
         'as'=> 'post.store'
     ]);
 
+    Route::get('/post/delete/{id}', [
+        'uses'=>'PostsController@destroy',
+        'as'=> 'post.delete'
+    ]);
+
+    Route::get('/posts', [
+        'uses'=>'PostsController@index',
+        'as'=>'posts'
+    ]);
+
+    Route::get('/posts/trashed', [
+        'uses'=>'PostsController@trashed',
+        'as'=>'posts.trashed'
+    ]);
+    Route::get('/post/kill/{id}', [
+        'uses'=>'PostsController@kill',
+        'as'=> 'post.kill'
+    ]);
+    Route::get('/post/restore/{id}', [
+        'uses'=>'PostsController@restore',
+        'as'=> 'post.restore'
+    ]);
+    Route::get('/post/edit/{id}', [
+        'uses'=>'PostsController@edit',
+        'as'=> 'post.edit'
+    ]);
+    Route::post('/post/update/{id}', [
+        'uses' => 'PostsController@update',
+        'as' => 'post.update'
+    ]);
     Route::get('/catagory.create', [
         'uses'=>'CatagoriesController@create',
         'as'=>'catagory.create'
@@ -61,5 +85,64 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
         'as'=>'catagory.update'
     ]);
 
+    Route::get('/tags', [
+        'uses'=>'TagsController@index',
+        'as'=>'tags'
+    ]);
+    Route::get('/tag/edit/{id}', [
+        'uses'=>'TagsController@edit',
+        'as'=>'tag.edit'
+    ]);
+    Route::get('/tag/create', [
+        'uses'=>'TagsController@create',
+        'as'=>'tag.create'
+    ]);
+    Route::post('/tag/store', [
+        'uses'=>'TagsController@store',
+        'as'=>'tag.store'
+    ]);
+    Route::post('/tag/update/{id}', [
+        'uses'=>'TagsController@update',
+        'as'=>'tag.update'
+    ]);
+    Route::get('/tag/delete/{id}', [
+        'uses'=>'TagsController@destroy',
+        'as'=>'tag.delete'
+    ]);
+
+    Route::get('/users', [
+        'uses'=>'UsersController@index',
+        'as'=>'users'
+    ]);
+    Route::get('/user/create', [
+        'uses'=>'UsersController@create',
+        'as'=>'user.create'
+    ]);
+    Route::post('/user/store', [
+        'uses'=>'UsersController@store',
+        'as'=>'user.store'
+    ]);
+    Route::get('/user/admin/{id}', [
+        'uses'=>'UsersController@admin',
+        'as'=>'user.admin'
+    ])->middleware('admin');
+    
+    Route::get('/user/not-admin/{id}', [
+        'uses'=>'UsersController@not_admin',
+        'as'=>'user.not.admin'
+    ]);
+    Route::get('user/profile', [
+        'uses' => 'ProfileController@index',
+        'as' => 'user.profile'
+    ]);
+    Route::get('user/delete/{id}', [
+        'uses' => 'UsersController@destroy',
+        'as' => 'user.delete'
+    ]);
+
+    Route::get('/user/profile/update', [
+        'user' => 'ProfileController@update',
+        'as' => 'user.profile.update'
+    ]);
 });
 
